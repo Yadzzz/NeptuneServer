0439ddd82d99be34f2fe6dc44b099f1f4a267503
Yad <yadmarzan@hotmail.com>
2022-12-18 19:19:29 +0100

    89888

--- NeptuneServer/Neptune/Applications/Application.cs
+++ NeptuneServer/Neptune/Applications/Application.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
 using NeptuneServer.Neptune.Applications.Logging;
+using NeptuneServer.Neptune.Client.Users;
+using NeptuneServer.Server.Database;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace NeptuneServer.Neptune.Applications
 {
@@ -14,7 +18,15 @@ namespace NeptuneServer.Neptune.Applications
 
         public void Log(Log log)
         {
-            
+            using (var command = new DatabaseCommand())
+            {
+                command.SetCommand("INSERT INTO application_logs () VALUES (@type, @text, @date)");
+                command.AddParameter("type", log.Type.ToString());
+                command.AddParameter("text", log.Text);
+                command.AddParameter("date", log.Date);
+
+                command.ExecuteQuery();
+            }
         }
     }
 }
