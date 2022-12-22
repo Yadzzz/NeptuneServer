768a9da30b43306d78d25269c3c106bb86f763b0
Yad <yadmarzan@hotmail.com>
2022-12-22 16:25:00 +0100

    Tests implemented

---
+++ NeptuneTests/ApplicationTests.cs
@@ -0,0 +1,27 @@
+﻿using NeptuneServer.Neptune.Applications;
+using NeptuneServer.Neptune.Client.Users;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeptuneTests
+{
+    public class ApplicationTests
+    {
+        [Test]
+        public void TryGetApplication()
+        {
+            if (ApplicationFactory.TryGetApplication(1, 1, out Application application))
+            {
+                Assert.IsTrue(application != null);
+                Assert.AreEqual(application.Id, 1);
+                Assert.AreEqual(application.UserId, 1);
+
+                Assert.Pass();
+            }
+        }
+    }
+}
---
+++ NeptuneTests/UsersTests.cs
@@ -0,0 +1,28 @@
+﻿using NeptuneServer.Neptune.Applications;
+using NeptuneServer.Neptune.Client.Users;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeptuneTests
+{
+    public class UsersTests
+    {
+        [Test]
+        public void TryGetUser()
+        {
+            if (UsersFactory.TryGetUser("sadasdasdasd", out User user))
+            {
+                Assert.IsTrue(user != null);
+
+                Assert.AreEqual(user.Id, 1);
+                Assert.AreEqual(user.Username, "Yad");
+
+                Assert.Pass();
+            }
+        }
+    }
+}
